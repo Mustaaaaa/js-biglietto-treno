32f5666c73522b49b8c8d73c2d28a8197a45f151
mustaaaaa <moustafaibrahim1512@gmail.com>
2024-03-04 16:53:25 +0100

    add

--- app.js
+++ app.js
@@ -6,20 +6,24 @@ oppure applicato uno sconto del 40% per gli over 65.
 L’output del prezzo finale va stampato in console in forma “umana” cioè con al massimo due decimali, per indicare centesimi sul prezzo. Questo richiederà un minimo di ricerca.*/
 
 /* inserisci l'età del passeggero e il totale dei km */
-const numero = parseInt(prompt('Età del passeggero')); //String
-const numero1 = parseFloat(prompt('Numero di km')); //String
+const eta = parseInt(prompt('Età del passeggero')); //Number
+const km = parseFloat(prompt('Numero di km')); //Number
 /* prezzo al km */
 let prezzoKm = 0.21; //Number
 /* prezzo di tutti i km percorsi */
-var prezzotot = (numero1 * prezzoKm);
+var prezzotot = (km * prezzoKm);
 
 /* sconto del 20% per i minorenni osconto del 40% per gli over 65 */
-if (18 <= numero <= 65) {
-    var prezzotot = prezzotot; //Number
+if (eta >= 65) {
+    var prezzototover = ((prezzotot * 60) / 100); //Number
 
-    console.log('Il prezzo è di ' + prezzotot.toFixed(2));
-}else if(numero < 18) {
-    var prezzotot = ((prezzotot * 80) / 100); //Number
+    console.log('Il prezzo è di ' + prezzototover.toFixed(2));
+}else if(eta < 18) {
+    var prezzototunder = ((prezzotot * 80) / 100); //Number
+    
+    console.log('Il prezzo è di ' + prezzototunder.toFixed(2));
+}else {
+    var prezzotot = prezzotot; //Number
     
     console.log('Il prezzo è di ' + prezzotot.toFixed(2));
 }
